f3715e7ae953ae54f117c1bb19845e0ae801a782
Haeryz <haeriz42069@gmail.com>
2026-04-27 06:16:22 +0700

    add ResearchSection to MainPage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import ExperienceSection from "@/components/sections/experience";
 import ProjectsSection from "@/components/sections/projects";
 import ContactSection from "@/components/sections/contact";
 import HeroSection from "@/components/sections/hero";
+import ResearchSection from "@/components/sections/research";
 
 function MainPage() {
   return (
@@ -18,6 +19,7 @@ function MainPage() {
         <HeroSection />
         <SkillsSection />
         <ExperienceSection />
+        <ResearchSection />
         <ProjectsSection />
         <ContactSection />
       </main>
