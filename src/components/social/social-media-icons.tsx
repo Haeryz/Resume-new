a6ee257e6b7c6e91b19f8d2b64b6892fe3834371
Haeryz <haeriz42069@gmail.com>
2026-04-27 06:18:01 +0700

    fix: remove unused social media icons from SocialMediaButtons component

--- src/components/social/social-media-icons.tsx
+++ src/components/social/social-media-icons.tsx
@@ -3,7 +3,7 @@
 import { useInView } from "motion/react";
 import React, { useRef } from "react";
 import { Button } from "../ui/button";
-import { SiGithub, SiInstagram, SiLinkedin, SiX } from "react-icons/si";
+import { SiGithub, SiLinkedin } from "react-icons/si";
 import { config } from "@/data/config";
 import Link from "next/link";
 
@@ -18,17 +18,7 @@ const BUTTONS = [
     href: config.social.linkedin,
     icon: <SiLinkedin size={"24"} color={"#fff"} />,
   },
-  {
-    name: "Twitter",
-    href: config.social.twitter,
-    icon: <SiX size={"24"} color={"#fff"} />,
-  },
-  {
-    name: "Instagram",
-    href: config.social.instagram,
-    icon: <SiInstagram size={"24"} color={"#fff"} />,
-  },
-];
+].filter((button) => button.href);
 
 const SocialMediaButtons = () => {
   const ref = useRef<HTMLDivElement>(null);
